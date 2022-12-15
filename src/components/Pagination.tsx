5800f02c0dc3e7a5453542c5a60aee25184b3d2a
Vinícius Rodrigues <jairrodriguesmd@gmail.com>
2022-12-15 15:22:56 -0300

    pagination bugfix

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -15,6 +15,9 @@ export function Pagination({
   page,
   productsPerPage,
 }: PaginationProps) {
+  let nextPage = data?.pagination.pageInfo.hasNextPage
+  let previousPage = data?.pagination.pageInfo.hasPreviousPage
+
   return (
     <div className="px-5 pt-5 flex flex-col xs:flex-row items-center xs:justify-between          ">
       <span
@@ -27,7 +30,7 @@ export function Pagination({
       <div className="inline-flex mt-2 xs:mt-0">
         <button
           onClick={() => {
-            if (data?.pagination.pageInfo.hasPreviousPage) {
+            if (data && previousPage) {
               setOffset(offset - productsPerPage);
               setPage(page - 1);
             }
@@ -42,7 +45,7 @@ export function Pagination({
         &nbsp; &nbsp;
         <button
           onClick={() => {
-            if (data?.pagination.pageInfo.hasNextPage) {
+            if (data && nextPage) {
               setOffset(offset + productsPerPage);
               setPage(page + 1);
             }
